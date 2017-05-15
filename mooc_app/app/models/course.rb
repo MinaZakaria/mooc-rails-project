f914b4d4054e18993705e2ae8f94f8f384dfc65b
MinaZakaria <engineer_minazakaria@yahoo.com>
2017-05-15 20:23:38 +0200

    validations

--- mooc_app/app/models/course.rb
+++ mooc_app/app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
   belongs_to :user
   has_many :lectures, :dependent => :destroy
+  validates :title, presence: true
+  validates :title, uniqueness: true
 end
--- mooc_app/app/models/user.rb
+++ mooc_app/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+    validate :birth_date_cannot_be_in_the_future
+    def birth_date_cannot_be_in_the_future
+        errors.add(:birth_date, "can't be in the future") if
+          !birth_date.blank? and birth_date > Date.today
+    end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
   has_many :courses
   has_many :comments
   mount_uploader :image, ImageUploader
